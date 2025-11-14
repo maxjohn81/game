aac7aaa2a746a4613c6ca0d47570740c113f93bc
maxjohn81 <rivoandriharisoa@gmail.com>
2025-11-14 11:40:40 +0300

    Terminée

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,50 @@
+import GradientButton from "./components/nurui/gradient-button";
 import SplashCursor from "./components/nurui/splash-cursor";
 
 function App() {
   return (
     <>
       <section className="bg-[url('/assets/game.jpg')] backdrop-blur-2xl bg-cover bg-center w-full h-screen">
-        <div className="w-ful max-w-[1500px] mx-auto px-5 md:px-10 lg:px-13 h-screen backdrop-blur-[1px] gap-5 flex justify-between flex-wrap items-center">
+        <div className="w-ful max-w-[1500px] mx-auto px-5 md:px-10 lg:px-13 h-screen backdrop-blur-[1px] flex justify-between flex-wrap items-center">
           <div>
             <img src="assets/img.png" alt="alt" />
           </div>
-          <div className="w-full md:w-sm bg-white/20 border border-white  h-100 rounded-2xl p-4 justify-items-center space-y-10">
+          <div className="w-full md:w-md bg-white/20 border border-white  h-auto rounded-2xl p-4 justify-items-center space-y-10">
             {" "}
-            <h1 className="text-8xl font-bold text-indigo-900">Games</h1>
-            <div className="grid gap-5 my-30 w-full">
-              <button className="w-full"><a className="p-4 w-full flex rounded-2xl justify-center items-center bg-pink-800 text-white text-2xl" href="https://super-mario-orpin.vercel.app/">Ghost Hunt👻⚔️</a></button>
-              <button className="w-full"><a className="p-4 w-full flex rounded-2xl justify-center items-center bg-pink-800 text-white text-2xl" href="https://odc-formation-jeux-interactif.vercel.app/">Robot Battle⚙️🤖</a></button>
+            <div className="w-full flex justify-center items-center">
+              <GradientButton />
+            </div>
+            <div className="grid gap-5 mt-30 w-full">
+              <button className="w-full hover:scale-105 duration-500">
+                <a
+                  className="p-4 w-full flex rounded-2xl justify-center items-center bg-pink-800 text-white text-2xl"
+                  href="https://super-mario-orpin.vercel.app/"
+                >
+                  Ghost Hunt👻⚔️
+                </a>
+              </button>
+
+              <button className="w-full hover:scale-105 duration-500">
+                <a
+                  className="p-4 w-full flex rounded-2xl justify-center items-center bg-pink-800 text-white text-2xl"
+                  href="https://odc-formation-jeux-interactif.vercel.app/"
+                >
+                  Robot Battle⚙️🤖
+                </a>
+              </button>
+              <button className="w-full hover:scale-105 duration-500">
+                <a
+                  className="p-4 w-full flex rounded-2xl justify-center items-center bg-pink-800 text-white text-2xl"
+                  href="https://fuse-lac.vercel.app"
+                >
+                  Galactic Rocket🚀
+
+                </a>
+              </button>
             </div>
           </div>
         </div>
-        <SplashCursor/>
+        <SplashCursor />
       </section>
     </>
   );
